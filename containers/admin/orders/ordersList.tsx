e7e4410e3faa23fcbd358b2a04e35f1409586f62
hajifattah <mortymtfp@yahoo.com>
2024-11-28 19:34:51 +0330

    create admin components and containers, create main header and footer

---
+++ containers/admin/orders/ordersList.tsx
@@ -0,0 +1,10 @@
+import { OrderCard } from "@/components/admin/orders/orderCard";
+
+export const OrdersList: React.FC = () => {
+  return (
+    <>
+      <OrderCard />
+      <h2>order card</h2>
+    </>
+  );
+};
