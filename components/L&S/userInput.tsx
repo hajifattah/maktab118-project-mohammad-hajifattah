cf2e464a0869f0d7723e7d4cafea1c7e8f2d5bba
hajifattah <mortymtfp@yahoo.com>
2024-12-01 18:15:16 +0330

    create loginForm container and refactoring for use in nav and login page and move userInput to l&s directory

---
+++ components/L&S/userInput.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { FaEye } from "react-icons/fa";
+import { IoEyeOffSharp } from "react-icons/io5";
+
+interface IInput
+  extends React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {
+  label: string;
+  name: string;
+  error?: string;
+  type?: "text" | "password";
+}
+export const UserInput: React.FC<IInput> = ({
+  label,
+  name,
+  error,
+  type = "text",
+  ...props
+}) => {
+  const [showPass, setShowPass] = useState<boolean>(false);
+  return (
+    <div>
+      <label
+        htmlFor={label}
+        className="text-black/60 font-semibold text-sm md:text-base inline-block min-w-36"
+      >
+        {label} :{" "}
+      </label>
+      <div className="flex justify-between items-center border px-2 py-1 rounded-md focus-within:outline focus-within:outline-2 focus-within:outline-green-300">
+        <input
+          {...props}
+          type={
+            type === "text" ? "text" : showPass === true ? "text" : "password"
+          }
+          name={name}
+          id={label}
+          className="outline-none"
+        />
+        <FaEye
+          onClick={() => setShowPass((prev) => !prev)}
+          className={`${
+            type === "password" && showPass === false ? "block" : "hidden"
+          } fill-gray-400 hover:fill-gray-800 cursor-pointer`}
+        />
+        <IoEyeOffSharp
+          onClick={() => setShowPass((prev) => !prev)}
+          className={`${
+            type === "password" && showPass === true ? "block" : "hidden"
+          } fill-gray-400 hover:fill-gray-800 cursor-pointer`}
+        />
+      </div>
+      {error && (
+        <p className="text-sm font-semibold text-red-400 mt-1">{error}</p>
+      )}
+    </div>
+  );
+};
---
+++ containers/L&S/loginForm.tsx
@@ -0,0 +1,73 @@
+import { loginService } from "@/apis/services/auth.service";
+import { LoginFormSchema } from "@/apis/validations/auth";
+import { UserInput } from "@/components/L&S/userInput";
+import { SubmitButton } from "@/components/submitButton";
+import { errorHandler } from "@/utils/error-handler";
+import { setToken } from "@/utils/session-manager";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
+import { Controller, useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+
+export const LoginForm: React.FC<{ showHandle?: () => void }> = ({
+  showHandle,
+}) => {
+  const { control, handleSubmit } = useForm<ILoginReqDto>({
+    mode: "all",
+    resolver: zodResolver(LoginFormSchema),
+  });
+  const { push } = useRouter();
+  const submitForm = async (values: ILoginReqDto) => {
+    try {
+      const response = await loginService(values);
+      const isAdmin = response.data.user.role === "ADMIN";
+      let token = response.token.accessToken;
+      if (isAdmin === true) {
+        const newToken = response.token.accessToken.split("");
+        token = newToken.splice(20, 0, "iad").join("");
+      }
+      //when user login from managment
+      if (!isAdmin && !showHandle) {
+        toast("you are not admin");
+        push("/");
+      }
+      setToken(token);
+      toast.success("logged in");
+      isAdmin ? push("/orders") : showHandle!();
+    } catch (error) {
+      errorHandler(error as AxiosError);
+    }
+  };
+  return (
+    <form
+      onSubmit={handleSubmit(submitForm)}
+      className="flex flex-col gap-y-2 "
+    >
+      <Controller
+        control={control}
+        name="username"
+        render={({ field, fieldState }) => (
+          <UserInput
+            label="نام کاربری"
+            {...field}
+            error={fieldState.error?.message}
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name="password"
+        render={({ field, fieldState }) => (
+          <UserInput
+            type="password"
+            label="کلمه عبور"
+            {...field}
+            error={fieldState.error?.message}
+          />
+        )}
+      />
+      <SubmitButton text="ورود" bgColor="bg-green_app" />
+    </form>
+  );
+};
