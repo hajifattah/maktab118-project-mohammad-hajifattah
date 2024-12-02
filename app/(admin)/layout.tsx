289ea13c7811ca3792ac9291428eb0461a4e0335
hajifattah <mortymtfp@yahoo.com>
2024-12-02 17:28:53 +0330

    create adminguard and add to admin rootlayout, complete nav for admin pages

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,17 +1,20 @@
 import { NavAdmin } from "@/components/admin/navAdmin";
 import { SideBarAdmin } from "@/components/admin/sideBarAdmin";
+import { AdminGuardProvider } from "@/providers/admin.Guard.Provider";
 
 const AdminLayout: React.FC<IChildren> = ({ children }) => {
   return (
-    <div className="flex ">
-      <SideBarAdmin />
-      <div className="w-full ">
-        <NavAdmin />
-        <div className="px-3 sm:px-8 py-4 mx-auto max-w-screen-2xl">
-        {children}
+    <AdminGuardProvider>
+      <div className="flex ">
+        <SideBarAdmin />
+        <div className="w-full ">
+          <NavAdmin />
+          <div className="px-3 sm:px-8 py-4 mx-auto max-w-screen-2xl">
+            {children}
+          </div>
         </div>
       </div>
-    </div>
+    </AdminGuardProvider>
   );
 };
 export default AdminLayout;
