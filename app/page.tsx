e1fa447a56cdd2822e5c75b42781ec0a75e61bc9
hajifattah <mortymtfp@yahoo.com>
2024-12-10 09:39:08 +0330

    add hero section to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
+import { HeroSection } from "@/containers/home/heroSection";
 import { MainLayout } from "@/containers/mainLayout";
 import { ProductsList } from "@/containers/products/productsList";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <MainLayout>
-      <div className="min-h-[calc(100vh-13rem)] py-6">
-        <ProductsList/>
+      <div className="min-h-[calc(100vh-13rem)] py-4 grid gap-y-4">
+        <HeroSection/>
+        <ProductsList />
       </div>
     </MainLayout>
   );
--- components/productCard.tsx
+++ components/productCard.tsx
@@ -1,20 +1,29 @@
 "use client";
 import Image from "next/image";
 import { useState } from "react";
+import { FaRegEye } from "react-icons/fa";
 import { HiOutlineShoppingBag } from "react-icons/hi";
 
 export const ProductCard: React.FC = () => {
   const [selected, setSelected] = useState<boolean>(false);
   return (
-    <div className={`flex flex-col gap-y-2 border-2 w-fit rounded-md shadow-lg bg-slate-50 ${selected ? "border-green_app" : "border-gray-300"}`
-    }>
-      <div className="relative mx-auto w-full rounded-md aspect-square">
-        <Image
-          alt="image"
-          src={"/product-Image.png"}
-          className="rounded-md"
-          fill
-        ></Image>
+    <div
+      className={`flex flex-col gap-y-2 border-2 w-fit rounded-md shadow-lg bg-slate-50 ${
+        selected ? "border-green_app" : "border-gray-300"
+      }`}
+    >
+      <div className="group relative ">
+        <div className="relative group-hover:blur-sm  mx-auto w-full rounded-md aspect-square">
+          <Image
+            alt="image"
+            src={"/product-Image.png"}
+            className="rounded-md"
+            fill
+          ></Image>
+        </div>
+        <div className="invisible text-gray-900 p-1 hover:text-slate-500 hover:cursor-pointer group-hover:visible  absolute left-[43%] top-[43%] z-20">
+          <FaRegEye className="size-10"/>
+        </div>
       </div>
       <div className="flex items-center justify-between px-3 py-2">
         <div className="text-sm">
@@ -24,7 +33,7 @@ export const ProductCard: React.FC = () => {
           <h2 className="text-gray-500 font-semibold">موجود در انبار</h2>
           <h2 className="font-semibold">250000 تومان</h2>
         </div>
-        <button onClick={()=>setSelected(prev=>!prev)}>
+        <button onClick={() => setSelected((prev) => !prev)}>
           <HiOutlineShoppingBag
             className={` rounded-full p-1 size-8  mr-1 hover:border hover:border-green_app ${
               selected ? "bg-green_app text-white" : "bg-gray-200 text-gray-800"
