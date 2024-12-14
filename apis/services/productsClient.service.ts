be5b5136ae8fed00244dbc52e2079d08eba4cc96
hajifattah <mortymtfp@yahoo.com>
2024-12-15 01:11:49 +0330

    edit client instance with axios interceptors for fetch and set access token with refresh token, and edit products client service with new instance, and then push to home and delete two token when ref token expires

--- apis/services/productsClient.service.ts
+++ apis/services/productsClient.service.ts
@@ -1,19 +1,18 @@
 "use client";
 
 import axios from "axios";
-import { clientAxiosInstance } from "../client.instance";
+import instance from "../client.instance";
 import { urls } from "../urls";
 
 type AddProductService = (values: FormData) => Promise<{ status: string }>;
 export const addProductService: AddProductService = async (values) => {
-  const instance = clientAxiosInstance();
+  // const instance = clientAxiosInstance();
   const response = await instance.post(urls.products.addProduct, values);
   return response.data;
 };
 
 type DeleteProductService = (id: string) => Promise<{ status: string }>;
 export const deleteProductService: DeleteProductService = async (id) => {
-  const instance = clientAxiosInstance();
   const response = await instance.delete(urls.products.deleteProduct(id));
   return response.data;
 };
@@ -23,7 +22,6 @@ type UpdateProductService = (
   data: FormData
 ) => Promise<{ status: string }>;
 export const updateProductService: UpdateProductService = async (id, data) => {
-  const instance = clientAxiosInstance();
   const response = await instance.patch(urls.products.updateProduct(id), data);
   return response.data;
 };
@@ -34,7 +32,6 @@ type UpdateProductPairsService = (
 export const updateProductPairsService: UpdateProductPairsService = async (
   pairs
 ) => {
-  const instance = clientAxiosInstance();
   const reqs = pairs.map((item) =>
     instance.patch(urls.products.updateProduct(item.id), item.pair)
   );
