f3ba2551e6c46661c160897ef4a5b78bcc35c1d9
hajifattah <mortymtfp@yahoo.com>
2024-12-18 09:26:45 +0330

    phase four completed

--- app/(user-pages)/shopping-card/page.tsx
+++ app/(user-pages)/shopping-card/page.tsx
@@ -3,7 +3,7 @@ import { ShoppingCardList } from "@/containers/shopping-card/shoppingCardList";
 
 const ShoppingCardPage: React.FC = () => {
   return (
-    <div className="flex flex-col lg:flex-row gap-x-2">
+    <div className="flex flex-col lg:flex-row gap-2">
       <ShoppingCardList />
       <TotalShoppingDetails/>
     </div>
