5c7c053247a5a76557b62eec8c5cfa8cade5144f
hajifattah <mortymtfp@yahoo.com>
2024-12-22 12:29:18 +0330

    create signup api, error handler for server error, set user detail to local storage when submit form

--- apis/services/auth.service.ts
+++ apis/services/auth.service.ts
@@ -7,3 +7,11 @@ export const loginService : ILoginService = async(body)=>{
     const response = await instance.post(urls.auth.login,body)
     return response.data;
 }
+
+
+type ISignupService = (body : ISignupReqDto)=> Promise<ILoginResDto>
+export const signupService : ISignupService = async(body)=>{
+    const instance = axiosInstance();
+    const response = await instance.post(urls.auth.signup,body)
+    return response.data;
+}
--- containers/L&S/signupForm.tsx
+++ containers/L&S/signupForm.tsx
@@ -1,86 +1,112 @@
-"use client"
-import { SignupFormSchema } from "@/apis/validations/auth.validation"
-import { TextareaInput } from "@/components/finalize-purchase/textareaInput"
-import { UserInput } from "@/components/L&S/userInput"
-import { SubmitButton } from "@/components/submitButton"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Controller, useForm } from "react-hook-form"
+"use client";
+import { signupService } from "@/apis/services/auth.service";
+import { SignupFormSchema } from "@/apis/validations/auth.validation";
+import { TextareaInput } from "@/components/finalize-purchase/textareaInput";
+import { UserInput } from "@/components/L&S/userInput";
+import { SubmitButton } from "@/components/submitButton";
+import { errorHandler } from "@/utils/error-handler";
+import { setRefToken, setToken, setUserInfo } from "@/utils/session-manager";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
+import { Controller, useForm } from "react-hook-form";
+import { toast } from "react-toastify";
 
-export const SignupForm : React.FC = ()=>{
-      const { control, handleSubmit } = useForm<ISignupReqDto>({
-        mode: "all",
-        resolver: zodResolver(SignupFormSchema),
+export const SignupForm: React.FC = () => {
+  const { control, handleSubmit } = useForm<ISignupReqDto>({
+    mode: "all",
+    resolver: zodResolver(SignupFormSchema),
+  });
+  const { push } = useRouter();
+  const submitForm = async (values: ISignupReqDto) => {
+    try {
+      const response = await signupService(values);
+      const token = response.token.accessToken;
+      const userData = response.data.user;
+      setToken(token);
+      setUserInfo({
+        id: userData._id,
+        firstName: userData.firstname,
+        lastName: userData.lastname,
+        address: userData.address,
+        phone: userData.phoneNumber,
       });
-    return <form
-    className="flex flex-col gap-y-2 "
-  >
-    <Controller
-      control={control}
-      name="firstname"
-      render={({ field, fieldState }) => (
-        <UserInput
-          label="نام"
-          {...field}
-          error={fieldState.error?.message}
-        />
-      )}
-    />
-    <Controller
-      control={control}
-      name="lastname"
-      render={({ field, fieldState }) => (
-        <UserInput
-          label="نام خانوادگی"
-          {...field}
-          error={fieldState.error?.message}
-        />
-      )}
-    />
-    <Controller
-      control={control}
-      name="username"
-      render={({ field, fieldState }) => (
-        <UserInput
-          label="نام کاربری"
-          {...field}
-          error={fieldState.error?.message}
-        />
-      )}
-    />
-    <Controller
-      control={control}
-      name="password"
-      render={({ field, fieldState }) => (
-        <UserInput
-          type="password"
-          label="کلمه عبور"
-          {...field}
-          error={fieldState.error?.message}
-        />
-      )}
-    />
-    <Controller
-      control={control}
-      name="phoneNumber"
-      render={({ field, fieldState }) => (
-        <UserInput
-          label="شماره همراه"
-          {...field}
-          error={fieldState.error?.message}
-        />
-      )}
-    />
-    <Controller
-      control={control}
-      name="address"
-      render={({ field, fieldState }) => (
-        <TextareaInput
-          label="آدرس"
-          {...field}
-          error={fieldState.error?.message}
-        />
-      )}
-    />
-    <SubmitButton text="ثبت نام" bgColor="bg-green_app" />
-  </form>
-}
+      setRefToken(response.token.refreshToken);
+      toast.success("ثبت نام با موفقیت انجام شد");
+      push("/");
+    } catch (error) {
+      errorHandler(error as AxiosError);
+    }
+  };
+  return (
+    <form
+      onSubmit={handleSubmit((values) => submitForm(values))}
+      className="flex flex-col gap-y-2 "
+    >
+      <Controller
+        control={control}
+        name="firstname"
+        render={({ field, fieldState }) => (
+          <UserInput label="نام" {...field} error={fieldState.error?.message} />
+        )}
+      />
+      <Controller
+        control={control}
+        name="lastname"
+        render={({ field, fieldState }) => (
+          <UserInput
+            label="نام خانوادگی"
+            {...field}
+            error={fieldState.error?.message}
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name="username"
+        render={({ field, fieldState }) => (
+          <UserInput
+            label="نام کاربری"
+            {...field}
+            error={fieldState.error?.message}
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name="password"
+        render={({ field, fieldState }) => (
+          <UserInput
+            type="password"
+            label="کلمه عبور"
+            {...field}
+            error={fieldState.error?.message}
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name="phoneNumber"
+        render={({ field, fieldState }) => (
+          <UserInput
+            label="شماره همراه"
+            {...field}
+            error={fieldState.error?.message}
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name="address"
+        render={({ field, fieldState }) => (
+          <TextareaInput
+            label="آدرس"
+            {...field}
+            error={fieldState.error?.message}
+          />
+        )}
+      />
+      <SubmitButton text="ثبت نام" bgColor="bg-green_app" />
+    </form>
+  );
+};
