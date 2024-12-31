54b77525f736ac638e31bbf8883c076448b8b431
hajifattah <mortymtfp@yahoo.com>
2025-01-01 01:55:59 +0330

    fetch orderProductsList and show with user and order details in checkOrder component in two mode

--- components/admin/orders/orderListCard.tsx
+++ components/admin/orders/orderListCard.tsx
@@ -6,17 +6,35 @@ export const OrderListCard: React.FC<IOrder> = async ({
   createdAt,
   user,
   deliveryStatus,
+  products,
+  deliveryDate,
 }) => {
   const userDtails = await fetchUserByIdService(user);
+
+  const date = new Date(createdAt);
+  const createdDate = createdAt.includes("1403")
+    ? createdAt.split("T")[0].replaceAll("-", "/")
+    : date.toLocaleDateString("fa-IR-u-nu-latn");
+  const delDate = new Date(createdAt);
+  const delivery = deliveryDate.includes("1403")
+    ? deliveryDate.split("T")[0].replaceAll("-", "/")
+    : delDate.toLocaleDateString("fa-IR-u-nu-latn");
+
   return (
     <tr className=" border-b bg-gray-800 border-gray-700 hover:bg-gray-600">
       <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
         {userDtails.data.user.firstname} {userDtails.data.user.lastname}
       </th>
       <td className="px-6 py-4">{totalPrice} تومان</td>
-      <td className="px-6 py-4">{createdAt.split("T")[0]}</td>
+      <td className="px-6 py-4">{createdDate}</td>
       <td className="px-6 py-4">
-      <CheckOrder mode={deliveryStatus ? "delivered":"notDelivered"}/>
+        <CheckOrder
+          deliveryDate={delivery}
+          createdAt={createdDate}
+          orderProducts={products}
+          userDetails={userDtails.data.user}
+          mode={deliveryStatus ? "delivered" : "notDelivered"}
+        />
       </td>
     </tr>
   );
--- containers/admin/orders/orderProducsList.tsx
+++ containers/admin/orders/orderProducsList.tsx
@@ -1,6 +1,35 @@
+import { fetchSingleProductListService } from "@/apis/services/products.service";
 import Link from "next/link";
+import { useCallback, useEffect, useState } from "react";
 
-export const OrderProductsList: React.FC = () => {
+export const OrderProductsList: React.FC<{
+  orderProducts: { product: string; count: number; _id: string }[];
+}> = ({ orderProducts }) => {
+  const [products, setProducts] = useState<
+    {
+      id: string;
+      name: string;
+      price: number;
+    }[]
+  >();
+  const callBack = useCallback(async () => {
+    const response = await fetchSingleProductListService(
+      orderProducts.map((item) => item.product)
+    );
+    const data = response.map((item) => {
+      const product = item.data.product;
+      return {
+        id: product._id,
+        name: product.name,
+        price: product.price,
+      };
+    });
+    setProducts(data);
+  }, [orderProducts]);
+
+  useEffect(() => {
+    callBack();
+  }, [orderProducts]);
   return (
     <>
       <table className="gap-y-4 rounded-md rtl:text-right w-full bg-blue_app max-h-[70vh] overflow-y-auto">
@@ -12,18 +41,35 @@ export const OrderProductsList: React.FC = () => {
           </tr>
         </thead>
         <tbody>
-          <tr className="text-slate-100 font-semibold">
-            <td>
-              <Link
-                href={""}
-                className="px-2 sm:px-6 py-3 w-full inline-block hover:text-green-200 underline"
-              >
-                شیر کم چرب
-              </Link>
-            </td>
-            <td className="px-2 sm:px-6 py-3">25000 تومان</td>
-            <td className="px-2 sm:px-6 py-3">5 عدد</td>
-          </tr>
+          {!products ? (
+            <tr>
+              <td className="pr-6 py-2 text-white rtl:text-right">
+                {" "}
+                درحال دریافت اطلاعات ...
+              </td>
+            </tr>
+          ) : (
+            products.map((item) => (
+              <tr key={item.id} className="text-slate-100 font-semibold">
+                <td>
+                  <Link
+                    href={`/products/${item.id}`}
+                    className="pxl-2 pr-6 sm:px-6 py-3 w-full inline-block hover:text-green-200 underline"
+                  >
+                    {item.name}
+                  </Link>
+                </td>
+                <td className="px-2 sm:px-6 py-3">{item.price} تومان</td>
+                <td className="px-2 sm:px-6 py-3">
+                  {
+                    orderProducts.find((item2) => item2.product === item.id)
+                      ?.count
+                  }{" "}
+                  عدد
+                </td>
+              </tr>
+            ))
+          )}
         </tbody>
       </table>
     </>
